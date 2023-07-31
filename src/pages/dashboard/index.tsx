40517509bdbb5c9c165772dbcd50d4383721dd99
abel-otegbola <abel15655@gmail.com>
2023-07-31 19:08:16 +0100

    feat: add leftbar in dashboard routes as global

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -2,16 +2,22 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import DashboardHome from "./dashboardHome/dashboardHome";
 import DashboardTopbar from "../../components/dashboardTopbar/dashboardTopbar";
 import Projects from "./projects/projects";
+import LeftBar from "../../components/leftbar/leftbar";
 
 function Dashboard() {
     return (
         <>
             <DashboardTopbar />
-            <Routes>
-                <Route path="/dashboard/" element={<Navigate to={"/dashboard/home"} />} />
-                <Route path="/dashboard/home" element={<DashboardHome />} />
-                <Route path="/dashboard/projects" element={<Projects />} />
-            </Routes>
+            <div className="flex">
+                <LeftBar />
+                <div className="flex-1">   
+                    <Routes>
+                        <Route path="/dashboard/" element={<Navigate to={"/dashboard/home"} />} />
+                        <Route path="/dashboard/home" element={<DashboardHome />} />
+                        <Route path="/dashboard/projects" element={<Projects />} />
+                    </Routes>
+                </div>
+            </div>
         </>
     )
 }
