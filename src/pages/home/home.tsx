4aa739ac0e572f2529f4c3dadefe1a8826633ef2
abel-otegbola <abel15655@gmail.com>
2023-07-31 05:41:33 +0100

    feat: add hero ui to homepage

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,11 +1,20 @@
 import Button from "../../components/button/button";
+import hero_img from "../../assets/hero_img.png"
 
 function Home()  {
     return (
-        <>
-            <h1 className="text-center p-[3%] text-3xl text-purple">Home</h1>
-            <Button text={"Get started"} icon={"o"} link={"#"} />
-        </>
+        <main>
+            <header className="flex flex-wrap jutify-between items-center">
+                <div className="md:w-[50%] w-full px-[5%] md:py-0 py-[10%]">
+                    <p className="p-1 rounded-full bg-slate-100/[0.05] w-fit border border-slate-100/[0.04] text-sm px-3 mb-3">A better world, one project at a time</p>
+                    <h1 className="py-[3%] text-[40px] bg-clip-text text-transparent bg-gradient-to-r from-purple to-green">Making Renewable Energy  more accessible, affordable and fun</h1>
+                    <p className="pb-6">Quest is built to help accelerate success by eliminating failure factors and providing success systems such as collaboration, accountability, time management and lots more.</p>
+                    <Button text={"Get started"} icon={"o"} link={"#"} />
+                </div>
+                
+                <img className="md:w-[50%] w-full" src={hero_img} alt="renewable energy sources illustration" />
+            </header>
+        </main>
     )
 }
 
