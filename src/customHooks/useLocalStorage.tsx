514511eff98c712fde09400346dc1c9dd7ab84aa
abel-otegbola <abel15655@gmail.com>
2023-08-01 21:00:10 +0100

    feat: create the uselocalStorage custom hook

---
+++ src/customHooks/useLocalStorage.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+const returnInitialState = (storageKey: string, initialState: any) => {
+    try {
+        const items = window.localStorage.getItem(storageKey);
+        return items ? JSON.parse(items) : initialState;
+    }
+    catch (error) {
+        return initialState;
+    }
+}
+
+
+export const useLocalStorage = (storageKey: string, initialState: any) => {
+    const [storedValue, setStoredValue] = useState(
+        returnInitialState(storageKey, initialState)
+    )
+
+    const setValue = (value: any) => {
+        try {
+            const valueToStore = value instanceof Function ? value(storedValue) : value;
+
+            window.localStorage.setItem(storageKey, JSON.stringify(valueToStore))
+
+            setStoredValue(valueToStore);
+        }
+        catch (error) {
+            console.log(error)
+        }
+    };
+
+    return [storedValue, setValue]
+}
