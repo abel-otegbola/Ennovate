ddc4f92697d17246d598dd1a923be783e6e23774
abel-otegbola <abel15655@gmail.com>
2023-08-01 21:46:06 +0100

    feat: add user info in leftbar component

--- src/components/leftbar/leftbar.tsx
+++ src/components/leftbar/leftbar.tsx
@@ -1,7 +1,8 @@
 import { FiTablet, FiBell, FiBox, FiPenTool, FiUser, FiUsers, FiLogOut, FiHeadphones, FiSettings } from "react-icons/fi";
 import { FaBars } from "react-icons/fa"
 import { useLocation } from "react-router-dom";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../customHooks/useAuth";
 
 interface Link {
     id: number; label: string; icon: any, link: string, msg: string
@@ -12,26 +13,35 @@ interface Links extends Array<Link>{}
 function LeftBar() {
     const [open, setOpen] = useState(false)
     const pathname = useLocation().pathname;
+    const { user } = useContext(AuthContext);
 
     const generalLinks: Links = [
         { id: 0, label: "Dashboard", icon: <FiTablet />, link: "/dashboard/home", msg: "" },
         { id: 1, label: "Projects", icon: <FiBox />, link: "/dashboard/projects", msg: "20" },
         { id: 2, label: "Learn", icon: <FiPenTool />, link: "/dashboard/learn", msg: "" },
-        { id: 2, label: "Launch", icon: <FiUser />, link: "/dashboard/myprojects", msg: "" },
-        { id: 2, label: "Collaborate", icon: <FiUsers />, link: "/dashboard/collaborate", msg: "" },
+        { id: 3, label: "Launch", icon: <FiUser />, link: "/dashboard/myprojects", msg: "" },
+        { id: 4, label: "Collaborate", icon: <FiUsers />, link: "/dashboard/collaborate", msg: "" },
     ]
     const userLinks: Links = [
         { id: 0, label: "Notifications", icon: <FiBell />, link: "/dashboard/notifications", msg: "" },
         { id: 1, label: "Settings", icon: <FiSettings />, link: "/settings", msg: "2" },
         { id: 2, label: "Help", icon: <FiHeadphones />, link: "/dashboard/help", msg: "" },
-        { id: 2, label: "Logout", icon: <FiLogOut />, link: "#", msg: "" }
+        { id: 3, label: "Logout", icon: <FiLogOut />, link: "#", msg: "" }
     ]
 
     return (
         <>
         <button className="md:hidden fixed z-30 top-0 left-0 p-5 text-lg opacity-[0.6] " onClick={() => setOpen(!open)}><FaBars /></button>
-        <div className={`xl:w-[18%] lg:w-[22%] md:w-[27%] py-[20px] text-[12px] h-screen md:sticky fixed top-[60px] left-0 bg-white dark:bg-black border border-transparent border-r-gray-200 dark:border-r-slate-100/[0.09] overflow-hidden z-10 transition-all duration-700 ${open ? " w-[240px]": "w-0"}`}>
-            
+        <div className={`xl:w-[18%] lg:w-[22%] md:w-[27%] pb-[10px] text-[12px] h-screen md:sticky fixed top-[60px] left-0 bg-white dark:bg-black border border-transparent border-r-gray-200 dark:border-r-slate-100/[0.09] overflow-hidden z-10 transition-all duration-700 ${open ? " w-[240px]": "w-0"}`}>
+            <div className="flex items-center my-2 gap-4 p-4">
+                <div className="h-[40px] w-[40px] rounded bg-slate-100 dark:bg-slate-200/[0.04]"></div>
+                <div className="text-[10px]">
+                    <h3>{user?.email}</h3>
+                    <div className="flex items-center gap-4">
+                        <p>{user?.displayName}</p>
+                    </div>
+                </div>                    
+            </div>
             {
                 generalLinks.map(link => {return (
                     <a key={link.id} href={link.link} className={`flex items-center justify-between w-full p-2 my-[1px] px-4 hover:bg-slate-100 dark:hover:bg-gray-200/[0.07] ${pathname === link.link ? "bg-gray-100 dark:bg-gray-200/[0.07] border border-transparent border-r-green text-green" : ""}`}>
