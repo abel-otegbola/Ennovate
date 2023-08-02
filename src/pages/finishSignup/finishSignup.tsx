571f662db2c4092dc7fc4fa5b88d66320acbd626
abel-otegbola <abel15655@gmail.com>
2023-08-02 09:17:59 +0100

    style: redesign finishSignup page and add username

--- src/pages/finishSignup/finishSignup.tsx
+++ src/pages/finishSignup/finishSignup.tsx
@@ -1,12 +1,12 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Button from "../../components/button/button";
 import { getAuth, isSignInWithEmailLink, signInWithEmailLink } from "firebase/auth";
 
 function FinishSignup() {
-    const [email, setEmail] = useState(localStorage.getItem('emailForSignin' || null));
+    const [email, setEmail] = useState(localStorage.getItem('emailForSignIn' || null));
     const [error, setError] = useState("")
 
-    useEffect(() => {
+    const signIn = () => {
         // Confirm the link is a sign-in with email link.
         const auth = getAuth();
         if (isSignInWithEmailLink(auth, window.location.href)) {
@@ -24,18 +24,25 @@ function FinishSignup() {
                 setError("Please input your email")
             }
         }
-    })
+    }
 
     return (
-        <div className="bg-white dark:bg-black w-full min-h-[500px] flex flex-col items-center justify-center">
-            <h1 className="text-xl my-3">Your email has been verified</h1>
-            <p className="my-2">Please continue to the dashboard</p>
-                    
-            <label htmlFor="email" className="block py-2 pt-4">Email:</label>
-            <input className="p-[12px] bg-transparent max-w-[300px] outline-none border border-gray-400/[0.3] focus:border-green/[0.5] rounded" id="email" name="email" type="email" placeholder="Enter your email" onChange={(e) => setEmail(e.target.value)} />
-            <p className="mt-2 text-[10px] text-red-500">{error}</p>
+        <div className="w-full min-h-[500px] flex flex-col items-center justify-center">
+            <div className="p-[40px] shadow-lg bg-white dark:bg-black rounded">
+                <h1 className="text-xl my-3">Your email has been verified</h1>
+                <p className="my-2">Please provide a username and profile image for your account</p>
+                        
+                <label htmlFor="email" className="block py-2 pt-4">Email:</label>
+                <input className="p-[12px] bg-transparent max-w-[300px] outline-none border border-gray-400/[0.3] focus:border-green/[0.5] rounded" id="email" name="email" disabled={email ? true : false} type="email" defaultValue={email ? email : ""} placeholder="Enter your email" onChange={(e) => setEmail(e.target.value)} />
 
-            <Button text={"Dashboard"} link={"/dashboard"} />
+                <label htmlFor="username" className="block py-2 pt-4">Username:</label>
+                <input className="p-[12px] bg-transparent max-w-[300px] outline-none border border-gray-400/[0.3] focus:border-green/[0.5] rounded" id="username" name="username" type="text" placeholder="Enter your username" onChange={(e) => setEmail(e.target.value)} />
+                <p className="mt-2 text-[10px] text-red-500">{error}</p>
+
+                <div onClick={() => signIn()} className="py-4">
+                    <Button text={"Continue"} link={"/dashboard"} />
+                </div>
+            </div>
         </div>
     )
 }
