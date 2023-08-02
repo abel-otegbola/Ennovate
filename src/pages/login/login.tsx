f24a4f0525e3365ffa90e23988878bf1219683b6
abel-otegbola <abel15655@gmail.com>
2023-08-02 09:17:28 +0100

    feat: add signin with google functionality

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,16 +1,19 @@
 import { FaFacebook, FaGoogle, FaLinkedin, FaTwitter } from "react-icons/fa";
-import { getAuth, sendSignInLinkToEmail } from "firebase/auth";
-import { useState } from "react";
+import { GoogleAuthProvider, getAuth, sendSignInLinkToEmail, signInWithPopup } from "firebase/auth";
+import { useEffect, useState } from "react";
 import { app } from "../../firebase/firebase";
+import Button from "../../components/button/button";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
     const [email, setEmail] = useState("")
     const [success, setSuccess] = useState("")
     const [error, setError] = useState({ code: "", msg: "" })
+    const navigate = useNavigate();
 
     const signup = () => {
         const actionCodeSettings = {
-            url: 'https://enquest.netlify.app/finishSignup',
+            url: `${import.meta.env.MODE === "development" ? "http://localhost:5173" : "https://enquest.netlify.app"}/finishSignup`,
             handleCodeInApp: true
         };
 
@@ -18,7 +21,7 @@ function Login() {
         sendSignInLinkToEmail(auth, email, actionCodeSettings)
         .then(() => {
             localStorage.setItem('emailForSignIn', email);
-            setSuccess("Verification email has been sent! Please check your mail to verify your email.")
+            setSuccess("Account activation link has been sent to the email address you provided.")
         })
         .catch((error) => {
             const code = error.code;
@@ -26,7 +29,28 @@ function Login() {
             setError({ code, msg })
         });
     }
+
+    const socialSignIn = (type: string) => {
+        if(type === "Google") {
+            const auth = getAuth(app);
+            const provider = new GoogleAuthProvider()
+            signInWithPopup(auth, provider)
+                .then(result => {
+                    const credential = GoogleAuthProvider.credentialFromResult(result);
+                    const token = credential?.accessToken;
+                    const user = result.user
+                    console.log(user, token)
+                    navigate("/dashboard")
+                })
+                .catch(error => {
+                    setError({ code: error.code, msg: error.message })
+                })
+        }
+    }
     
+    useEffect(() => {
+        console.log(import.meta.env.MODE)
+    }, [])
 
     return (
             <div className="w-full flex flex-wrap jutify-between bg-white dark:bg-black items-center px-[3%] py-[5%]">
@@ -38,7 +62,7 @@ function Login() {
                         {
                             ['Google', 'Facebook', 'Twitter', 'Linkedin'].map((social,i )=> {
                                 return (
-                                    <button key={i} className="flex items-center justify-center gap-4 p-[12px] border border-gray-200 dark:border-gray-100/[0.09] w-full rounded hover:border-green hover:dark:border-green hover:text-green">
+                                    <button key={i} onClick={() => socialSignIn(social)} className="flex items-center justify-center gap-4 p-[12px] border border-gray-200 dark:border-gray-100/[0.09] w-full rounded hover:border-green hover:dark:border-green hover:text-green">
                                         { social === "Google" ? <FaGoogle /> : social === "Facebook" ? <FaFacebook /> : social === "Twitter" ? <FaTwitter /> : social === "Linkedin" ? <FaLinkedin /> : "" }
                                         {social}
                                     </button>
@@ -49,11 +73,24 @@ function Login() {
 
                     {
                         error.msg ? 
-                        <div className="bg-white dark:bg-black absolute top-0 left-0 w-full h-full flex items-center justify-center text-red-500">{error.msg}</div> : "" 
+                        <div className="absolute bg-white/[0.8] dark:bg-black/[0.9] backdrop-blur-sm top-0 left-0 w-full h-full flex items-center justify-center">
+                            <div className="bg-white dark:bg-black p-[40px] m-[5%] shadow-lg rounded">
+                                <h2 className="mb-3 text-2xl">An error occured</h2>
+                                <p className="py-2 mb-4">{error.msg}</p>
+                                <div onClick={() => setError({code: '', msg: ""})}>
+                                    <Button text={"Try again"} link={"#"} />
+                                </div>
+                            </div>
+                        </div> : "" 
                     }
                     {
                         success ? 
-                        <div className="bg-white dark:bg-black absolute top-0 left-0 w-full h-full flex items-center justify-center text-green-500">{success}</div> : "" 
+                        <div className="absolute bg-white/[0.8] dark:bg-black/[0.9] backdrop-blur-sm top-0 left-0 w-full h-full flex items-center justify-center">
+                            <div className="bg-white dark:bg-black p-[40px] m-[5%] shadow-lg rounded">
+                                <h2 className="mb-3 text-2xl">Verify your account</h2>
+                                <p>{success}</p>
+                            </div>
+                        </div> : "" 
                     }
 
                     <label htmlFor="email" className="block py-2 pt-4">Signin with Email:</label>
