45752b7a00cb664c9ae4e4582c9361f83564fc48
abel-otegbola <abel15655@gmail.com>
2023-08-02 14:06:42 +0100

    feat: add logout effect in leftbar

--- src/components/leftbar/leftbar.tsx
+++ src/components/leftbar/leftbar.tsx
@@ -3,6 +3,8 @@ import { FaBars } from "react-icons/fa"
 import { useLocation } from "react-router-dom";
 import { useContext, useState } from "react";
 import { AuthContext } from "../../customHooks/useAuth";
+import { getAuth, signOut } from "firebase/auth";
+import { app } from "../../firebase/firebase";
 
 interface Link {
     id: number; label: string; icon: any, link: string, msg: string
@@ -14,6 +16,8 @@ function LeftBar() {
     const [open, setOpen] = useState(false)
     const pathname = useLocation().pathname;
     const { user } = useContext(AuthContext);
+
+    const auth = getAuth(app)
 
     const generalLinks: Links = [
         { id: 0, label: "Dashboard", icon: <FiTablet />, link: "/dashboard/home", msg: "" },
@@ -54,7 +58,7 @@ function LeftBar() {
             <p className="border border-transparent border-t-gray-100/[0.07] my-5 w-full"></p>
             {
                 userLinks.map(link => {return (
-                    <a key={link.id} href={link.link} className={`flex items-center justify-between w-full p-2 my-[1px] px-4 hover:bg-slate-100 dark:hover:bg-gray-200/[0.07] ${pathname === link.link ? "bg-gray-100 dark:bg-gray-200/[0.07] border border-transparent border-r-green text-green" : ""}`}>
+                    <a key={link.id} href={link.link} onClick={() => link.label === "Logout" ? signOut(auth) : ""} className={`flex items-center justify-between w-full p-2 my-[1px] px-4 hover:bg-slate-100 dark:hover:bg-gray-200/[0.07] ${pathname === link.link ? "bg-gray-100 dark:bg-gray-200/[0.07] border border-transparent border-r-green text-green" : ""}`}>
                         <span className="w-[30px] text-[14px]">{link.icon}</span>
                         <span className="flex-1 p-2 break-normal">{link.label}</span>
                         <span className="p-[0px] px-2 rounded text-[10px] bg-purple/[0.1]">{link.msg}</span>
