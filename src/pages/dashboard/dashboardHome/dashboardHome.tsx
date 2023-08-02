1a9db046b24fca8e2cc4eda0cb6249d7c12202de
abel-otegbola <abel15655@gmail.com>
2023-08-02 11:23:35 +0100

    fix: remove demo img, move dashboard ui to projects

--- src/pages/dashboard/dashboardHome/dashboardHome.tsx
+++ src/pages/dashboard/dashboardHome/dashboardHome.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import ProjectGrid from "../../../components/projectGrid/projectGrid"
+import Button from "../../../components/button/button"
 
 interface Category {
     id: number, img: any, title: string, info: string
@@ -8,52 +8,40 @@ interface Categories extends Array<Category>{}
 
 function DashboardHome() {
     const [active, setActive] = useState("Wind Power")
-
-    const categories: Categories = [
-        { id: 0, img: "./", title: "Wind Power", info: "Wind turbine projects which involves using wind as source of energy" },
-        { id: 1, img: "./", title: "Solar Thermal Energy", info: "Wind turbine projects which involves using wind as source of energy" },
-        { id: 2, img: "./", title: "Biomass", info: "Wind turbine projects which involves using wind as source of energy" },
-        { id: 3, img: "./", title: "Green Hydrogen", info: "Wind turbine projects which involves using wind as source of energy" },
-        { id: 4, img: "./", title: "Biofuel", info: "Wind turbine projects which involves using wind as source of energy" },
-        { id: 5, img: "./", title: "Nuclear Energy", info: "Wind turbine projects which involves using wind as source of energy" },
-        { id: 6, img: "./", title: "Hydro Electricity", info: "Wind turbine projects which involves using wind as source of energy" },
-    ]
-    
     const Projects: Categories = [
-        { id: 0, img: "https://www.bing.com/images/blob?bcid=qHlJMURYFOcFgg", title: "Wind Power", info: "Wind turbine projects which involves using wind as source of energy" },
-        { id: 1, img: "https://www.bing.com/images/blob?bcid=qHlJMURYFOcFgg", title: "Solar Thermal Energy", info: "Wind turbine projects which involves using wind as source of energy" },
-        { id: 2, img: "https://www.bing.com/images/blob?bcid=qHlJMURYFOcFgg", title: "Biomass", info: "Wind turbine projects which involves using wind as source of energy" },
-        { id: 3, img: "https://www.bing.com/images/blob?bcid=qHlJMURYFOcFgg", title: "Green Hydrogen", info: "Wind turbine projects which involves using wind as source of energy" },
-        { id: 4, img: "https://www.bing.com/images/blob?bcid=qHlJMURYFOcFgg", title: "Biofuel", info: "Wind turbine projects which involves using wind as source of energy" },
-        { id: 5, img: "https://www.bing.com/images/blob?bcid=qHlJMURYFOcFgg", title: "Nuclear Energy", info: "Wind turbine projects which involves using wind as source of energy" },
-        { id: 6, img: "https://www.bing.com/images/blob?bcid=qHlJMURYFOcFgg", title: "Hydro Electricity", info: "Wind turbine projects which involves using wind as source of energy" },
+        { id: 0, img: "", title: "Wind Power", info: "Wind turbine projects which involves using wind as source of energy" },
+        { id: 1, img: "", title: "Solar Thermal Energy", info: "Wind turbine projects which involves using wind as source of energy" },
+        { id: 2, img: "", title: "Biomass", info: "Wind turbine projects which involves using wind as source of energy" },
+        { id: 3, img: "", title: "Green Hydrogen", info: "Wind turbine projects which involves using wind as source of energy" },
+        { id: 4, img: "", title: "Biofuel", info: "Wind turbine projects which involves using wind as source of energy" },
+        { id: 5, img: "", title: "Nuclear Energy", info: "Wind turbine projects which involves using wind as source of energy" },
+        { id: 6, img: "", title: "Hydro Electricity", info: "Wind turbine projects which involves using wind as source of energy" },
     ]
 
     return (
         <div className="p-[3%] w-full">
-            <h1 className="px-2">Renewable Energy Sources</h1>
+            <div className="flex justify-between">
+                <h1 className="px-2 text-lg">Explore Newest Renewable Energy Projects</h1>
+                <Button text={"Create New Project"} link={"/dashboard/create"} />
+            </div>
+            
             <div className="w-full flex gap-4 border border-transparent border-y-gray-200 dark:border-y-gray-100/[0.1] bg-white dark:bg-slate-100/[0.01] p-2 my-4 text-[12px] overflow-x-auto scrollbar">
                 {
-                    categories.map(category => {
+                    Projects.map(project => {
                         return (
-                            <div key={category.id} className={`${active === category.title ? "text-green" : "hover:text-green"}`} onClick={() => setActive(category.title)}>
-                                <div className={`h-[150px] w-[200px] bg-slate-100 dark:bg-slate-200/[0.08] cursor-pointer rounded ${active === category.title ? "border border-green/[0.5]" : "hover:border hover:border-green/[0.5]"}`}></div>
-                                <h2 className="p-2">{category.title}</h2>
+                            <div key={project.id} className={`${active === project.title ? "text-green" : "hover:text-green"}`} onClick={() => setActive(project.title)}>
+                                <div className={`h-[150px] md:w-[300px] w-full bg-slate-200 dark:bg-slate-200/[0.08] cursor-pointer rounded ${active === project.title ? "border border-green/[0.5]" : "hover:border hover:border-green/[0.5]"}`}></div>
+                                <h2 className="p-2">{project.title}</h2>
                             </div>
                         )
                     })
                 }
             </div>
             
-            <h3 className="px-2 text-transparent bg-clip-text bg-gradient-to-b from-purple to-green mt-8">{active}</h3>
+            
+            <h1 className="px-2 text-lg mt-8 py-2">Trending Renewable Energy Projects</h1>
             <div className="py-2">
-                {
-                    Projects.filter(item => item.title === active).map(project => {
-                        return (
-                            <ProjectGrid key={project.id} id={project.id} img={project.img} title={project.title} info={project.info} />
-                        )
-                    })
-                }
+                
             </div>
 
         </div>
--- src/pages/dashboard/projects/projects.tsx
+++ src/pages/dashboard/projects/projects.tsx
@@ -1,7 +1,61 @@
+import { useState } from "react"
+import ProjectGrid from "../../../components/projectGrid/projectGrid"
+
+interface Category {
+    id: number, img: any, title: string, info: string
+}
+interface Categories extends Array<Category>{}
+
 function Projects() {
+    const [active, setActive] = useState("Wind Power")
+
+    const categories: Categories = [
+        { id: 0, img: "./", title: "Wind Power", info: "Wind turbine projects which involves using wind as source of energy" },
+        { id: 1, img: "./", title: "Solar Thermal Energy", info: "Wind turbine projects which involves using wind as source of energy" },
+        { id: 2, img: "./", title: "Biomass", info: "Wind turbine projects which involves using wind as source of energy" },
+        { id: 3, img: "./", title: "Green Hydrogen", info: "Wind turbine projects which involves using wind as source of energy" },
+        { id: 4, img: "./", title: "Biofuel", info: "Wind turbine projects which involves using wind as source of energy" },
+        { id: 5, img: "./", title: "Nuclear Energy", info: "Wind turbine projects which involves using wind as source of energy" },
+        { id: 6, img: "./", title: "Hydro Electricity", info: "Wind turbine projects which involves using wind as source of energy" },
+    ]
+    
+    const Projects: Categories = [
+        { id: 0, img: "", title: "Wind Power", info: "Wind turbine projects which involves using wind as source of energy" },
+        { id: 1, img: "", title: "Solar Thermal Energy", info: "Wind turbine projects which involves using wind as source of energy" },
+        { id: 2, img: "", title: "Biomass", info: "Wind turbine projects which involves using wind as source of energy" },
+        { id: 3, img: "", title: "Green Hydrogen", info: "Wind turbine projects which involves using wind as source of energy" },
+        { id: 4, img: "", title: "Biofuel", info: "Wind turbine projects which involves using wind as source of energy" },
+        { id: 5, img: "", title: "Nuclear Energy", info: "Wind turbine projects which involves using wind as source of energy" },
+        { id: 6, img: "", title: "Hydro Electricity", info: "Wind turbine projects which involves using wind as source of energy" },
+    ]
+
     return (
-        <div>
-            projects
+        <div className="p-[3%] w-full">
+            <h1 className="px-2 text-lg py-2">Renewable Energy Sources</h1>
+            <div className="w-full flex gap-4 border border-transparent border-y-gray-200 dark:border-y-gray-100/[0.1] bg-white dark:bg-slate-100/[0.01] p-2 my-4 text-[12px] overflow-x-auto scrollbar">
+                {
+                    categories.map(category => {
+                        return (
+                            <div key={category.id} className={`${active === category.title ? "text-green" : "hover:text-green"}`} onClick={() => setActive(category.title)}>
+                                <div className={`h-[150px] w-[200px] bg-slate-200 dark:bg-slate-200/[0.08] cursor-pointer rounded ${active === category.title ? "border border-green/[0.5]" : "hover:border hover:border-green/[0.5]"}`}></div>
+                                <h2 className="p-2">{category.title}</h2>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            
+            <h3 className="px-2 text-transparent bg-clip-text bg-gradient-to-b from-purple to-green mt-12">{active}</h3>
+            <div className="py-2">
+                {
+                    Projects.filter(item => item.title === active).map(project => {
+                        return (
+                            <ProjectGrid key={project.id} id={project.id} img={project.img} title={project.title} info={project.info} />
+                        )
+                    })
+                }
+            </div>
+
         </div>
     )
 }
