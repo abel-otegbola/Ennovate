4946b9dd54d619787ca3987c23e7944c1ac91ca3
abel-otegbola <abel15655@gmail.com>
2023-08-02 09:18:39 +0100

    feat: add custom env config in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,28 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import checker from 'vite-plugin-checker';
 
+
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({command, mode}) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    define: {
+      __APP_ENV__: env.local
+    },
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'], // <==
+      setupFiles: './setupTests.ts', // <==
+    },
+    plugins: [
+      react(),
+      checker({ typescript: true }),
+    ],
+  }
   // NEW
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'], // <==
-    setupFiles: './setupTests.ts', // <==
-  },
-  plugins: [
-    react(),
-    checker({ typescript: true }),
-  ],
+  
 })
 
