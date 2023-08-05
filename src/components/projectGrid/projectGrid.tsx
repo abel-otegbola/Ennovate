86e7d3fd540b79effc27cedf16be1a9ac91f2d9e
abel-otegbola <abel15655@gmail.com>
2023-08-05 03:09:40 +0100

    fix: project image src and sizes

--- src/components/projectGrid/projectGrid.tsx
+++ src/components/projectGrid/projectGrid.tsx
@@ -11,21 +11,24 @@ function ProjectGrid({ project }: any ) {
                 <div className="flex items-center my-2 gap-4">
                     <div className="h-[40px] w-[40px] rounded bg-slate-100 dark:bg-slate-200/[0.04]"></div>
                     <div className="text-[10px]">
-                        <h3>Abel Otegbola</h3>
+                        <h3>User</h3>
                         <div className="flex items-center gap-4">
                             <p>12 Jan 2023</p>
                             <p className="text-purple/[0.6]">Collaborate</p>
                         </div>
                     </div>                    
                 </div>
-                <div className={`flex justify-center items-center w-full min-h-[200px] bg-slate-200 dark:bg-slate-200/[0.04] cursor-pointer rounded hover:border hover:border-green hover:bg-green/[0.2]`}>
-                    <img src={project.img} className="w-full max-h-[300px]" />
+                <div className={`flex justify-center items-center w-full md:h-[300px] h-[250px] bg-slate-200 dark:bg-slate-200/[0.04] cursor-pointer rounded hover:border hover:border-green hover:bg-green/[0.2]`}>
+                    <img src={project.img.url} className="w-full h-full object-cover" />
                 </div>
                 <h2 className="py-2">{project.title}</h2>
                 <p className="text-[12px]">{project.info}</p>
             </div>
-            <div className={`lg:static text-[12px] lg:h-auto h-full absolute top-0 right-0 bg-slate-200 dark:bg-[#1d1d23] overflow-hidden transition-all duration-700 ${openSide ? "lg:w-[50%] w-[75%]" : "lg:w-[50%] w-0"}`}>
-                <p className="p-4">other details</p>
+            <div className={`lg:static text-[12px] lg:h-auto h-full absolute top-0 right-0 bg-slate-200 dark:bg-[#1d1d23] overflow-hidden overflow-y-auto transition-all duration-700 ${openSide ? "lg:w-[50%] w-[75%]" : "lg:w-[50%] w-0"}`}>
+                <p className="p-4">Description:</p>
+                <p className="px-4">{project.description}</p>
+                <p className="p-4">Procedures:</p>
+                <p className="px-4">{project.procedures}</p>
             </div>
         </div>
     )
