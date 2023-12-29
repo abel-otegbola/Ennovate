a55bc3468f9250fcb7c0fdf1cc1cdf7896aff87d
abel-otegbola <abel15655@gmail.com>
2023-12-29 15:00:13 +0100

    style: change heading in footer to uppercase

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,21 +1,21 @@
 function Footer() {
     return (
         <div className="">
-            <div className="grid lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-[30px] py-[30px] md:px-[9%] px-[3%] border border-transparent border-t-gray-200/[0.09]">
+            <div className="grid lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-[30px] py-[30px] md:px-[9%] px-[3%] border border-transparent border-t-gray-700/[0.09]">
                 <ul className="w-full">
-                    <h2 className="py-2 text-lg font-bold">Products</h2>
+                    <h2 className="py-2 font-semibold uppercase">Products</h2>
                     <li className="flex w-full"><a href="/explore" className="py-[5px] w-full hover:text-green">Projects</a></li>
                     <li className="flex w-full"><a href="https://sdgs.un.org" className="py-[5px] w-full hover:text-green">Learn</a></li>
                     <li className="flex w-full"><a href="/dashboard/create" className="py-[5px] w-full hover:text-green">Create</a></li>
                 </ul>
                 <ul className="w-full">
-                    <h2 className="py-2 text-lg font-bold">Support</h2>
+                    <h2 className="py-2 font-semibold uppercase">Support</h2>
                     <li className="flex w-full"><a href="/FAQs" className="py-[5px] w-full hover:text-green">FAQs</a></li>
                     <li className="flex w-full"><a href="/terms&conditions" className="py-[5px] w-full hover:text-green">Terms of Service</a></li>
                     <li className="flex w-full"><a href="/privacypolicy" className="py-[5px] w-full hover:text-green">Privacy Policy</a></li>
                 </ul>
                 <ul className="w-full">
-                    <h2 className="py-2 text-lg font-bold">Resources and Blogs</h2>
+                    <h2 className="py-2 font-semibold uppercase">Resources and Blogs</h2>
                     <li className="flex w-full"><a href="/dashboard/projects" className="py-[5px] w-full hover:text-green">Project Resources</a></li>
                     <li className="flex w-full"><a href="https://sdgs.un.org/goals/goal7" className="py-[5px] w-full hover:text-green">SDG goal7 - Access to affordable, reliable, sustainable and modern energy</a></li>
                     <li className="flex w-full"><a href="https://sdgs.un.org/events/hlpf-2023-side-event-youth-engagement-systemic-transformation-sdg-summit-implementing-global" className="py-[5px] w-full hover:text-green">Youth engagement for systemic transformation at SDG-summit</a></li>
@@ -27,7 +27,7 @@ function Footer() {
                     <p className="py-1">Support@ennovate.com</p>
                 </ul>
             </div>
-            <div className="bg-gray-200 dark:bg-gray-100/[0.09] text-center">
+            <div className="bg-gray-900 text-white text-center">
                 <p className="p-[3%]">Copyright &copy; {new Date().getFullYear()}</p>
             </div>
         </div>
