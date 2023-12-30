3d385064a6ed509e405c5d9c22b72051a540ff22
abel-otegbola <abel15655@gmail.com>
2023-12-30 03:41:42 +0100

    refactor: remove excess states and codes in upload

--- src/components/upload/upload.tsx
+++ src/components/upload/upload.tsx
@@ -1,18 +1,22 @@
 // import { storage } from "../../firebase/storage";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { handleUpload, storage } from "../../firebase/storage";
 import { deleteObject, getDownloadURL, uploadBytesResumable } from "firebase/storage";
 import { ref } from "firebase/storage";
 
-function Upload({ images, setImages, accept, id }: any) {
-    const [status, setStatus] = useState("not loaded")
+function Upload({ images, setImages, accept, id, i }: any) {
     const [uploadStatus, setUploadStatus] = useState({status: "", percent: 0})
-    const [img, setImg] = useState<any>({id: "", name: "", type: "", url: "" })
+
+    useEffect(() => {
+        if(images[i].url) {
+            setUploadStatus({status: "uploaded", percent: 100})
+        }
+    }, [])
 
     const uploadImg = () => {
-        handleUpload(img.file)
+        handleUpload(images[i].file)
         .then(result => {
-            const uploadTask = uploadBytesResumable(result, img.file);
+            const uploadTask = uploadBytesResumable(result, images[i].file);
             uploadTask.on(
             "state_changed",
             (snapshot) => {
@@ -29,8 +33,8 @@ function Upload({ images, setImages, accept, id }: any) {
                 getDownloadURL(uploadTask.snapshot.ref)
                 .then((url) => {
                     setUploadStatus({status: "finished", percent: 100});
-                    let newImg = { id, name: img.name, url, type: img.type}  
-                    setImg(newImg) 
+                    let newImg = { id, name: images[i].name, url, type: images[i].type}  
+                    // setImg(newImg) 
                     setImages(images.map((image: any) => {
                         if(image.id === id) {
                             return newImg
@@ -39,7 +43,7 @@ function Upload({ images, setImages, accept, id }: any) {
                             return image
                         }
                     }))       
-                    setStatus("uploaded")
+                    setUploadStatus({status: "uploaded", percent: 100})
                 });
             })
         })
@@ -51,8 +55,14 @@ function Upload({ images, setImages, accept, id }: any) {
         const file = e.target.files[0];
 
         reader.onloadend = function () {
-            setImg({id, name: file.name, url: reader.result, type: file.type, file})
-            
+            setImages(images.map((image: any) => {
+                if(image.id === id) {
+                    return {id, name: file.name, url: reader.result, type: file.type, file}
+                }
+                else {
+                    return image
+                }
+            }))    
         }
         
         if (file) {
@@ -63,10 +73,10 @@ function Upload({ images, setImages, accept, id }: any) {
     }
 
     const deleteImg = () => {
-        const desertRef = ref(storage, `files/${img.name}`);
+        const desertRef = ref(storage, `files/${images[i].name}`);
         // Delete the file
         deleteObject(desertRef).then(() => {
-            setImg({ id, name: "", type: "", url: "" })
+            // setImg({ id, name: "", type: "", url: "" })
             setImages(images.map((image: any) => {
                 if(image.id === id) {
                     return { id, name: "", type: "", url: "" }
@@ -85,50 +95,46 @@ function Upload({ images, setImages, accept, id }: any) {
     return (
         <div className="flex flex-1">
                 <div className="p-4 rounded">
-                    <div>
-                        <img src={img.url} alt={img.name} className={`max-w-[200px] w-full rounded object-cover`} />
-                    </div>
-                    
-                    <div className="px-4 text-[12px]">
-                        <div className="flex items-center my-2 gap-2">
+                    <div className="relative min-h-[150px] w-[200px] rounded">
+                        <img src={images[i].url} alt={images[i].name} className={`max-w-[200px] w-full rounded object-cover`} />
+                        <div className="absolute top-0 left-0 rounded w-full h-full">
                             {
-                                status === "preview" ?
-                                    <div className="flex gap-2">
-                                        {
-                                            uploadStatus.status === "finished" ? "" :
-                                            <div onClick={uploadImg}>
-                                                <button className="p-[6px] px-4 rounded border border-gray-500/[0.2]">Upload</button>
-                                            </div>
-                                        }
-                                        <button className="p-[2px] px-4 rounded border border-red-500/[0.6]" onClick={() => { setImg({id , name: "", url: "", type: ""}); setStatus("not loaded"); }}>Delete</button>
-                                    </div>
-                                : status === "uploaded" ?
-                                    <div className="flex gap-2">
-                                        <button className="p-[2px] px-4 rounded border border-red-500/[0.6]" onClick={() => { setStatus("not loaded"); deleteImg() }}>Delete</button>
+                                uploadStatus.status !== "not loaded" && uploadStatus.status !== "preview" && uploadStatus.status && uploadStatus.status !== "uploaded" ? 
+                                    <div className="flex flex-col justify-center items-center bg-white/[0.8] dark:bg-black/[0.7] backdrop-blur-sm p-4 w-full h-full">
+                                        <p className="mb-2">{uploadStatus?.status} : {uploadStatus?.percent}%</p>
+                                        <p className="w-full min-h-[10px] rounded-lg bg-slate-200">
+                                            <p className={`min-h-[10px] rounded-lg bg-green`} style={{ width: `${uploadStatus?.percent}%` }}></p>
+                                        </p>
                                     </div>
                                 :
+                                ""
+                            }
+                        </div>
+                    </div>
+                    
+                    <div className="text-[12px]">
+                        <div className="flex items-center my-2 gap-2">
+                            {
+                                uploadStatus.status === "preview" ?
+                                <>
+                                    <button className="p-[2px] px-4 rounded border border-gray-500/[0.2]" onClick={uploadImg}>Upload</button>
+                                    <button className="p-[2px] px-4 rounded border border-red-500/[0.6]" onClick={() => { setImages(images.filter((item: any) => item.id === id)); setUploadStatus({status: "not loaded", percent: 0}); }}>Delete</button>
+                                </>
+                                : 
+                                uploadStatus.status === "uploaded" || uploadStatus.status === "finished"?
+                                    <button className="p-[2px] px-4 rounded border border-red-500/[0.6]" onClick={() => { setUploadStatus({status: "not loaded", percent: 0}); deleteImg() }}>Delete</button>
+                                
+                                :
                                 <label htmlFor={`file${id}`}>
-                                    <span className="p-[6px] px-4 text-[10px] rounded border border-gray-500/[0.2] cursor-pointer">Choose image</span>
-                                    <input className="bg-gray-50 dark:bg-gray-900 hidden" accept={accept} id={`file${id}`} type="file" onChange={(e) => { previewFile(e); setStatus("preview") }} />
+                                    <span className="p-[10px] px-4 text-[10px] rounded border border-gray-500/[0.2] cursor-pointer">Choose image</span>
+                                    <input className="bg-gray-50 dark:bg-gray-900 hidden" accept={accept} id={`file${id}`} type="file" onChange={(e) => { previewFile(e); setUploadStatus({status:"preview", percent: 0}) }} />
                                 </label>
                             }
                         </div>
                     </div>
                     
                 </div>
-                <div className="w-[30%] my-3 gap-2">
-                {
-                    uploadStatus.status && status !== "not loaded" ? 
-                        <div className="m-4 w-[40%]">
-                            <p className="mb-2">{uploadStatus?.status} : {uploadStatus?.percent}%</p>
-                            <p className="w-full min-h-[10px] rounded-lg bg-slate-200">
-                                <p className={`min-h-[10px] rounded-lg bg-green`} style={{ width: `${uploadStatus?.percent}%` }}></p>
-                            </p>
-                        </div>
-                    :
-                    ""
-                }
-                </div>
+                
         </div>
     )
 }
